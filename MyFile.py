156ac4d39d7d6b970170eed82469df86d50c49a8
Narfanta <nawwartohme@gmail.com>
2017-02-01 10:26:51 -0800

    Done with DrawPoly

--- MyFile.py
+++ MyFile.py
@@ -1,4 +1,5 @@
 import turtle
+sven = turtle.Turtle()
 # AddTen(n):  Adds 10 to the parameter n and returns the result
 def AddTen(n):
     x = n + 10
@@ -10,3 +11,13 @@ def DrawRectangle(Anyturtle, l, w):
         Anyturtle.right(90)
         Anyturtle.forward(l)
         Anyturtle.right(90)
+# DrawPoly(Anyturtle, n):  Will draw a regular polygon with 'n' sides of
+  #* You should select the size of the polygon so that it always fits in the screen
+def DrawPoly(Anyturtle, n):
+    Anyturtle.pu()
+    Anyturtle.backward(300)
+    Anyturtle.left(90)
+    Anyturtle.pd()
+    for i in range(n):
+        Anyturtle.forward(5)
+        Anyturtle.right(360/n)
