4d1c5299ea940b76f231098882cadef03ad7a5c9
Narfanta <nawwartohme@gmail.com>
2017-02-01 10:40:27 -0800

    done with Function.py

--- Function.py
+++ Function.py
@@ -1,10 +1,13 @@
 """Define a function that will take a parameter, n, and triple it and return
 the result"""
-
-
-
-
+def Triple(n):
+    x = n*3
+    return x
+print(Triple(5))
 
 """Write a program that will prompt the user for an input value (n) and print
 the result of 3n by calling the function defined above.  Make sure you include
 the necessary print statements and address any issues with whitespace. """
+print('Please type in a NUMBER to triple.')
+x = int(input())
+print(Triple(x))
