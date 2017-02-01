0a3988cf1eae4368058e7c0fff64dff99e053884
Narfanta <nawwartohme@gmail.com>
2017-02-01 10:15:04 -0800

    done with drawrectangle

--- MyFile.py
+++ MyFile.py
@@ -1,4 +1,12 @@
+import turtle
 # AddTen(n):  Adds 10 to the parameter n and returns the result
 def AddTen(n):
     x = n + 10
     return x
+#DrawRectangle(Anyturtle, l, w):
+def DrawRectangle(Anyturtle, l, w):
+    for i in range(2):
+        Anyturtle.forward(w)
+        Anyturtle.right(90)
+        Anyturtle.forward(l)
+        Anyturtle.right(90)
